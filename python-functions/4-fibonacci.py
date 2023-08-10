861119ece9af5761884ce77a9c1a976de042dbe8
cvuakdgreat <cvuak19@gmail.com>
2023-08-10 06:41:54 -0700

    this is task 3 to task 5

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    fib_sequence = []
+    a, b = 0, 1
+
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+
+    return fib_sequence
