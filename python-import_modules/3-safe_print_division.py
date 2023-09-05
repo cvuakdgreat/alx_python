57571a780b790388222cb445a38d20db7ec76544
cvuakdgreat <cvuak19@gmail.com>
2023-09-04 17:09:41 -0700

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    if __name__ == "__main__":
-        try:
-            result = a / b
-            print("inside result : {}".format(result))
-        except ZeroDivisionError:
-            result = None
-            print("inside result : {}".format(result))
-        finally:
-            print("{} / {} = {}".format(a, b, result) )
+    try:
+        result = a / b
+        print("inside result : {}".format(result))
+    except ZeroDivisionError:
+        result = None
+        print("inside result : {}".format(result))
+    finally:
+        print("{} / {} = {}".format(a, b, result) )
