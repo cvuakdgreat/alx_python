93fd716a8458c6cb1177b2ae3e0af31118c5920c
cvuakdgreat <cvuak19@gmail.com>
2023-09-27 19:19:14 +0100

    2-Square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -19,7 +19,7 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size <= 0:
+        if size < 0:
             raise ValueError("size must be > 0")
         self.__size = size
         
@@ -33,4 +33,4 @@ def area(self):
         Returns:
             this returns the Area of the square
     """
-    return self.__size **2
+    return self.size **2
