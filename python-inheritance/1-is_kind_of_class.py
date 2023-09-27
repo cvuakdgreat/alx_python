68ebf1c9a9ee6a66de60fe9fffad3a9896663f80
cvuakdgreat <cvuak19@gmail.com>
2023-09-27 20:22:11 +0100

    task 2

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -4,7 +4,7 @@ def is_kind_of_class(obj, a_class):
 	"""Function that returns True or false if the object is exactly an instance of the specified class or Inherited class""" 	
 	if type(obj) == a_class:
 		return True
-	elif type(self.obj) == a_class:
-		return True
+	# elif type(self.obj) == a_class:
+	# 	return True
 	else:
 		return False
