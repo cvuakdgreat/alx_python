1d0ddad1ccb503c38567cfab476bcc97bc232c04
cvuakdgreat <cvuak19@gmail.com>
2023-09-27 20:13:40 +0100

    Task 0 & 1

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,3 @@
+""""Method that returns True or false if the object is exactly an instance of the specified class """
+def is_same_class(obj, a_class):
+	return type(obj) == a_class
---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,7 @@
+def is_kind_of_class(obj, a_class):
+	if type(obj) == a_class:
+		return True
+	elif type(self.obj) == a_class:
+		return True
+	else:
+		return False
