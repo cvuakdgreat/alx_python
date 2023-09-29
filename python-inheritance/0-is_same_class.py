e346462d3a957bd5528a36e8ae7e7b95b97c16f7
cvuakdgreat <cvuak19@gmail.com>
2023-09-29 14:18:47 +0100

    task 0

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,3 +2,7 @@
 def is_same_class(obj, a_class):
 	"""Function that returns True or false if the object is exactly an instance of the specified class """ 
 	return type(obj) == a_class
+
+	def ignore_init_subclass_methods(cls):
+    # Define a function to filter out methods starting with '__init_subclass__'
+	    return [attr for attr in dir('cls') if not attr.startswith('__init_subclass__')]
