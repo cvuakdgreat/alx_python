3e28043bb8feb3250fed425807ee118cf741e35d
cvuakdgreat <cvuak19@gmail.com>
2023-09-29 11:02:46 +0100

    task 7

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,27 @@
-""" getting BaseGeometry Class for the module 5-base_geometry """
+"""	Creating a Class called BaseGeometry"""
+class BaseGeometry:
+	""" Class called Base Geometry"""
+	def __init__(self):
+		pass
+	
+	def area(self):
+		""" method that raises a Exception"""
+		raise Exception("area() is not implemented")
+	
+	def integer_validator(self, name, value):
+		""" Method that validates intergers
+		
+			args:
+				value 
 
-from 5-base_geometry import BaseGeometry
+			return:
+				TypeError
+				valueError
+		"""
+		if not isinstance(value, int):
+			raise TypeError("{} must be an integer".format(name))
+		if value <= 0 :
+			raise ValueError("{} must be greater than 0".format(name))
 
 """creating a class called Rectangle"""
 
