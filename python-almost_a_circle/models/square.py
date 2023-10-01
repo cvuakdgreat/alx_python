910cbbcc4ed1cfb04e84166851ec50f40d3f3fd5
cvuakdgreat <cvuak19@gmail.com>
2023-10-01 18:17:38 +0100

    Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,25 +11,25 @@ class Square(Rectangle):
         """
         This  method takes in the size and cordinate of a square
         """
-        super().__init__(size, size, x=0, y=0, id=None)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """
         This method overides the other methods
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
     
     @property
     def size(self):
         """
         This getter method is used to retrive values of the attribute width
         """
-        return self.width
+        return self.size
     @size.setter
     
     def size(self, value):
         """
         This setter method modifies the value of the width
         """
-        self.width = value
+        self.size = value
 
