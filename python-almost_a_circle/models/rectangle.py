24505b4d25e69aaa84fc3dc310738b85d3f99a16
cvuakdgreat <cvuak19@gmail.com>
2023-10-01 18:06:15 +0100

    Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -189,8 +189,47 @@ class Rectangle(Base):
 		"""
 			method that represents a rectangle with #
 		"""
+		for _ in range(self.__y):
+			print()
 		for _ in range(self.__height):
-			print("#" * self.__width)
+			print(" " * self.__x + "#" * self.__width)
+
+	def __str__(self):
+		""""
+		Method to overwrite the __str__ return
+		"""
+		return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+	def update(self, *args, **kwargs):
+		"""
+		Method assigns argunments to each attributes
+		"""
+
+		args_length = len(args)
+		kwargs_lenght = len(kwargs)
 
 
-	
+		if args_length > 0:
+			self.id = args[0]
+		if args_length > 1:
+			self.width = args[1]
+		if args_length > 2:
+			self.height = args[2]
+
+		if args_length > 3:
+			self.x = args[3]
+		if args_length > 4:
+			self.y = args[4]
+
+	for key, value in kwargs.items():
+		if key == "id":
+			self.id= value
+		elif key == "width":
+			self.width = value
+		elif key == "height":
+			self.height = value
+		elif key == "x":
+			self.x = value
+		elif key == "y":
+			self.y = value
+
