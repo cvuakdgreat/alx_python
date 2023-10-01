c0466b36446f429c7945d21302b1f1e803d07d2a
cvuakdgreat <cvuak19@gmail.com>
2023-10-01 18:07:35 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -221,15 +221,15 @@ class Rectangle(Base):
 		if args_length > 4:
 			self.y = args[4]
 
-	for key, value in kwargs.items():
-		if key == "id":
-			self.id= value
-		elif key == "width":
-			self.width = value
-		elif key == "height":
-			self.height = value
-		elif key == "x":
-			self.x = value
-		elif key == "y":
-			self.y = value
+		for key, value in kwargs.items():
+			if key == "id":
+				self.id= value
+			elif key == "width":
+				self.width = value
+			elif key == "height":
+				self.height = value
+			elif key == "x":
+				self.x = value
+			elif key == "y":
+				self.y = value
 
